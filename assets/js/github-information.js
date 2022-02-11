c19ca423f1d653ef7f55fad677de1e2993f51c27
Cintakins <jacintalerwill@hotmail.com>
2022-02-11 13:10:21 +0000

    fixed some bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,10 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
+
     var username = $("#gh-username").value();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a Github username!</h2>`);
@@ -69,3 +73,4 @@ function fetchGitHubInformation(event) {
             }
         )
 }
+$(document).ready(fetchGitHubInformation);
